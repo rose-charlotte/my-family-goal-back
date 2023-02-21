294cbe75560e7d1324965b515e1d7fa5d7d73738
francois <francoisgrunert@gmail.com>
2023-02-21 12:03:57 +0100

    route signin

--- app/controllers/authentificationController.js
+++ app/controllers/authentificationController.js
@@ -1,8 +1,31 @@
 import { authentificationDatamapper } from "../datamappers/index.js";
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 const authentificationController = {
     async signin(req, res){
-        
+        try {
+            // find user with email
+            const email = req.body.email
+            const user = await authentificationDatamapper.signin(email);
+            if(!user) throw new Error('No user found');
+            
+            // check password
+            const password = req.body.password;
+            const isValidPassword = await bcrypt.compare(password, user.password);
+            if (user && !isValidPassword) throw new Error('Invalid Password');
+
+            // No error : User find, password valid
+            // delete password property of user object
+            delete user.password;
+            // save user in session
+            req.session.user = user;
+            // create token jwt
+            const token = jwt.sign(user, process.env.SESSION_SECRET, {expiresIn: '1h'});
+            return res.json({token});
+        } catch (error) {
+            return res.status(500).json(error.message);
+        }
     }
 }
 
--- app/datamappers/authentificationDatamapper.js
+++ app/datamappers/authentificationDatamapper.js
@@ -1,7 +1,12 @@
 import {client} from "../services/database.js";
 
 const authentificationDatamapper = {
-
+    async signin(email){
+        const sql = `SELECT * FROM "user" WHERE email = $1`;
+        const values = [email];
+        const result = await client.query(sql, values);
+        return result.rows[0];
+    }
 }
 
 export {authentificationDatamapper};
