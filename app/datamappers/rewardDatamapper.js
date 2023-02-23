7e9aa1305211e421cfdd48c8a83a4c00252f7aad
francois <francoisgrunert@gmail.com>
2023-02-23 18:39:38 +0100

    routes /reward

--- app/datamappers/rewardDatamapper.js
+++ app/datamappers/rewardDatamapper.js
@@ -9,6 +9,46 @@ const rewardDatamapper = {
         const values = [form.title, form.price, familyId];
         const result = await client.query(sql, values);
         return result.rows[0];
+    },
+    
+    async update(form, id){
+        const sql = `
+            UPDATE reward
+            SET title = $1, price = $2, "isPurchase" = $3
+            WHERE id = $4
+            RETURNING *;`
+        const values = [form.title, form.price, form.isPurchase, id];
+        const result = await client.query(sql, values);
+        return result.rows[0];
+    },
+
+    async delete(id){
+        const sql = `DELETE FROM reward WHERE id = $1;`
+        const values = [id];
+        const result = await client.query(sql, values);
+        return result.rowCount;
+    },
+
+    async purchase(id){
+        const sql = `
+            UPDATE reward
+            SET "isPurchase" = $1
+            WHERE id = $2
+            RETURNING *;`
+        const values = [true, id];
+        const result = await client.query(sql, values);
+        return result.rows[0];
+    },
+
+    async updateCredit(userId, familyId, price){
+        const sql = `
+            UPDATE user_has_family
+            SET credit = credit - $1
+            WHERE user_id = $2 AND family_id = $3
+            RETURNING credit;`
+        const values = [price, userId, familyId];
+        const result = await client.query(sql, values);
+        return result.rows[0];
     }
 }
 
--- app/routers/rewardRouter.js
+++ app/routers/rewardRouter.js
@@ -3,5 +3,8 @@ const rewardRouter = express.Router();
 import { rewardController } from "../controllers/index.js";
 
 rewardRouter.post('/family/:id/reward', rewardController.create);
+rewardRouter.patch('/reward/:id', rewardController.update);
+rewardRouter.delete('/reward/:id', rewardController.delete);
+rewardRouter.patch('/reward/:rewardId/user/:userId', rewardController.purchase);
 
 export {rewardRouter};
